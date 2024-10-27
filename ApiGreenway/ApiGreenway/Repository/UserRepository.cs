ba94f19db518bfba789ba7f8546651b838226c06
marceloamellopaixao <marceloamellopaixao@gmail.com>
2024-10-27 19:46:59 -0300

    fix: Correção do UserRepository

--- ApiGreenway/ApiGreenway/Repository/UserRepository.cs
+++ ApiGreenway/ApiGreenway/Repository/UserRepository.cs
@@ -19,23 +19,13 @@ public class UserRepository : IUserRepository
 
     public async Task<IEnumerable<User>> GetUsers()
     {
-        var users = await _dbContext.Users.Where(u => u.ds_uid_fb != null && u.dt_finished_at == null).ToListAsync();
-        if (users == null)
-        {
-            throw new Exception("Não há usuários cadastrados!");
-        }
-
+        var users = await _dbContext.Users.Where(u => u.dt_finished_at == null).ToListAsync() ?? throw new Exception("Não há usuários cadastrados!");
         return users;
     }
 
     public async Task<User> GetUserById(int UserId)
     {
-        var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.id_user == UserId && u.ds_uid_fb != null && u.dt_finished_at == null);
-        if (user == null)
-        {
-            throw new Exception("Usuário não encontrado ou excluído!");
-        }
-
+        var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.id_user == UserId && u.dt_finished_at == null) ?? throw new Exception("Usuário não encontrado ou excluído!");
         return user;
     }
 }
