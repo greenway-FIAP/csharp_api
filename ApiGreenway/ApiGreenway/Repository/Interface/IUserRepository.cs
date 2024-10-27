58d6da4a8abfd477b07756b522e135f5a91b9ca3
marceloamellopaixao <marceloamellopaixao@gmail.com>
2024-10-27 01:39:39 -0300

    fix: Correção do UserRepository

--- ApiGreenway/ApiGreenway/Repository/Interface/IUserRepository.cs
+++ ApiGreenway/ApiGreenway/Repository/Interface/IUserRepository.cs
@@ -7,10 +7,4 @@ public interface IUserRepository
 {
     Task<IEnumerable<User>> GetUsers();
     Task<User> GetUserById(int UserId);
-
-    //Task<User> AddUser(User user);
-    //Task<User> Login(string ds_email);
-
-    Task<User> UpdateUser(UserUpdateDto user);
-    void DeleteUser(int UserId);
 }
--- ApiGreenway/ApiGreenway/Repository/UserRepository.cs
+++ ApiGreenway/ApiGreenway/Repository/UserRepository.cs
@@ -19,70 +19,23 @@ public class UserRepository : IUserRepository
 
     public async Task<IEnumerable<User>> GetUsers()
     {
-        return await _dbContext.Users.Where(u => u.dt_finished_at == null).ToListAsync();
+        var users = await _dbContext.Users.Where(u => u.ds_uid_fb != null && u.dt_finished_at == null).ToListAsync();
+        if (users == null)
+        {
+            throw new Exception("Não há usuários cadastrados!");
+        }
+
+        return users;
     }
 
     public async Task<User> GetUserById(int UserId)
     {
-        return await _dbContext.Users.FirstOrDefaultAsync(u => u.id_user == UserId && u.dt_finished_at == null);
-    }
-
-    /*
-    public async Task<User> AddUser(User user)
-    {
-        // Verifica se o Email já está cadastrado
-        if (await _dbContext.Users.AnyAsync(u => u.ds_email == user.ds_email && u.dt_finished_at == null))
+        var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.id_user == UserId && u.ds_uid_fb != null && u.dt_finished_at == null);
+        if (user == null)
         {
-            throw new Exception("E-mail já cadastrado!");
+            throw new Exception("Usuário não encontrado ou excluído!");
         }
 
-        // Hash da Senha
-        user.ds_password = BCrypt.Net.BCrypt.HashPassword(user.ds_password);
-
-        // Adiciona o User no Banco de Dados
-        var userDb = await _dbContext.Users.AddAsync(user);
-        await _dbContext.SaveChangesAsync();
-
-        return userDb.Entity;
-    }
-
-    // Login
-    public async Task<User> Login(string ds_email)
-    {
-        return await _dbContext.Users.FirstOrDefaultAsync(u => u.ds_email == ds_email && u.dt_finished_at == null);
-    }
-
-    */
-
-    public async Task<User> UpdateUser(UserUpdateDto user)
-    {
-        var userDb = await _dbContext.Users.FirstOrDefaultAsync(u => u.id_user == user.id_user);
-        if (userDb == null)
-        {
-            return null; // Retorna null se o User não for encontrado
-        }
-
-        userDb.ds_email = user.ds_email;
-
-        // Atualiza a senha, garantindo que ela seja criptografada... Caso esteja vazia não atualiza
-        if (!string.IsNullOrEmpty(user.ds_password))
-        {
-            userDb.ds_password = BCrypt.Net.BCrypt.HashPassword(user.ds_password);
-        }
-
-        userDb.dt_updated_at = DateTimeOffset.UtcNow.ToOffset(TimeSpan.FromHours(-3)); // UTC-3 Brasília
-
-        await _dbContext.SaveChangesAsync();
-        return userDb;
-    }
-
-    public async void DeleteUser(int UserId)
-    {
-        var userDb = await _dbContext.Users.FirstOrDefaultAsync(u => u.id_user == UserId);
-        if (userDb != null)
-        {
-            userDb.dt_finished_at = DateTimeOffset.UtcNow.ToOffset(TimeSpan.FromHours(-3)); // UTC-3 Brasília
-            await _dbContext.SaveChangesAsync();
-        }
+        return user;
     }
 }
