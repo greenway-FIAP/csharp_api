c20d0542e8f9bf599ebcb47c70a3f978a6f81baa
marceloamellopaixao <marceloamellopaixao@gmail.com>
2024-10-27 00:40:42 -0300

    fix: Corrigido o UserUpdateDTO

--- ApiGreenway/ApiGreenway/Models/Dtos/UserUpdateDto.cs
+++ ApiGreenway/ApiGreenway/Models/Dtos/UserUpdateDto.cs
@@ -2,8 +2,10 @@
 
 namespace ApiGreenway.Models.Dtos;
 
-public class UserUpdateDto : User
+public class UserUpdateDto
 {
+    [JsonIgnore]
+    public int id_user { get; set; }
     public string? ds_email { get; set; }
     public string? ds_password { get; set; }
 }
