84b8042c86aebd0df401fac53cdb01d1c52c9522
marceloamellopaixao <marceloamellopaixao@gmail.com>
2024-10-27 01:39:08 -0300

    fix: Correção do que solicita para o usuário no Tipo de Usuário.

--- ApiGreenway/ApiGreenway/Models/UserType.cs
+++ ApiGreenway/ApiGreenway/Models/UserType.cs
@@ -13,9 +13,12 @@ public class UserType
 
     public required string ds_title { get; set; }
 
+    [JsonIgnore]
     public DateTimeOffset dt_created_at { get; set; } = DateTimeOffset.UtcNow.ToOffset(TimeSpan.FromHours(-3)); // UTC-3 Brasília
 
+    [JsonIgnore]
     public DateTimeOffset? dt_updated_at { get; set; }
 
+    [JsonIgnore]
     public DateTimeOffset? dt_finished_at { get; set; }
 }
