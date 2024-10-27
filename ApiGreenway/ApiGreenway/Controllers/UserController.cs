a7bd0e2872b1bfb5dde6a2c00628e0c398086ed1
marceloamellopaixao <marceloamellopaixao@gmail.com>
2024-10-27 01:39:57 -0300

    fix: Correção do UserController

--- ApiGreenway/ApiGreenway/Controllers/UserController.cs
+++ ApiGreenway/ApiGreenway/Controllers/UserController.cs
@@ -1,15 +1,11 @@
 ﻿using ApiGreenway.Models;
 using ApiGreenway.Models.Dtos;
-using ApiGreenway.Repository;
 using ApiGreenway.Repository.Interface;
 using ApiGreenway.Services.Authentication;
-using FirebaseAdmin.Auth;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ApiGreenway.Controllers
 {
@@ -19,14 +15,45 @@ namespace ApiGreenway.Controllers
     {
         private readonly IUserRepository _userRepository;
         private readonly IAuthService _authService;
-        private readonly IConfiguration _configuration;
 
-        public UserController(IUserRepository userRepository, IAuthService authService, IConfiguration configuration)
+        /// <summary>
+        /// Construtor para a classe UserController.
+        /// </summary>
+        /// <param name="userRepository">Repositório de usuários.</param>
+        /// <param name="authService">Serviço de autenticação.</param>
+        public UserController(IUserRepository userRepository, IAuthService authService)
         {
-            this._userRepository = userRepository;
-            this._authService = authService;
-            this._configuration = configuration;
+            _userRepository = userRepository;
+            _authService = authService;
         }
+
+        /// <summary>
+        /// Faz login de um usuário.
+        /// </summary>
+        /// <param name="request">Os dados do usuário a ser autenticado.</param>
+        /// <returns>Um token JWT se o login for bem-sucedido.</returns>
+        /// <response code="200">Login bem-sucedido.</response>
+        /// <response code="400">E-mail ou senha inválidos.</response>
+        /// <response code="500">Erro ao realizar login.</response>
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login([FromBody] UserLoginDTO request)
+        {
+            try
+            {
+                if (request == null || string.IsNullOrEmpty(request.ds_email) || string.IsNullOrEmpty(request.ds_password))
+                {
+                    return BadRequest("E-mail ou senha inválidos.");
+                }
+
+                string token = await _authService.LoginAsync(request);
+                return Ok(token);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao realizar login: {ex.Message}");
+            }
+        }
+
 
         /// <summary>
         /// Obtém todos os usuários.
@@ -42,21 +69,21 @@ namespace ApiGreenway.Controllers
                 var users = await _userRepository.GetUsers();
                 return Ok(users);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao recuperar os dados do Banco de Dados");
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao recuperar os dados do Banco de Dados: {ex.Message}");
             }
         }
 
         /// <summary>
         /// Obtém um usuário pelo ID.
         /// </summary>
-        /// <param name="userId">O ID do usuário.</param>
+        /// <param name="userId">O ID do usuário a ser recuperado.</param>
         /// <returns>O usuário correspondente ao ID.</returns>
         /// <response code="200">Retorna o usuário encontrado.</response>
         /// <response code="404">Usuário não encontrado.</response>
         /// <response code="500">Erro ao recuperar os dados do banco de dados.</response>
-        [HttpGet("{userId}")]
+        [HttpGet("{userId:int}")]
         public async Task<ActionResult<UserDetailedDTO>> GetUserById(int userId)
         {
             try
@@ -64,155 +91,74 @@ namespace ApiGreenway.Controllers
                 var user = await _userRepository.GetUserById(userId);
                 if (user == null)
                 {
-                    return NotFound();
+                    return NotFound($"Usuário com o ID: {userId}, não foi encontrado!");
                 }
 
                 return Ok(user);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao recuperar os dados do Banco de Dados");
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao recuperar os dados do Banco de Dados: {ex.Message}");
             }
         }
-        /*
+
         /// <summary>
         /// Registra um novo usuário.
         /// </summary>
-        /// <param name="user">O usuário a ser registrado.</param>
-        /// <returns>O usuário criado.</returns>
+        /// <param name="request">Os dados do usuário a ser registrado.</param>
+        /// <returns>O UID do usuário criado.</returns>
         /// <response code="201">Usuário criado com sucesso.</response>
         /// <response code="400">Dados inválidos.</response>
         /// <response code="500">Erro ao adicionar dados no banco de dados.</response>
         [HttpPost("register")]
-        public async Task<ActionResult<User>> AddUser(User user)
-        {
-            try
-            {
-                if (user == null)
-                {
-                    return BadRequest("Alguns dados estão inválidos, verifique!!");
-                }
-
-                var createdUser = await _userRepository.AddUser(user);
-                return CreatedAtAction(nameof(GetUserById), new
-                {
-                    userId = createdUser.id_user
-                }, createdUser);
-            }
-            catch (Exception)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao adicionar dados no Banco de Dados");
-            }
-        }
-
-        /// <summary>
-        /// Faz login de um usuário.
-        /// </summary>
-        /// <param name="user">O usuário a ser autenticado.</param>
-        /// <returns>Um token JWT se o login for bem-sucedido.</returns>
-        /// <response code="200">Login bem-sucedido.</response>
-        /// <response code="400">E-mail ou senha inválidos.</response>
-        [HttpPost("login")]
-        public async Task<ActionResult<string>> Login(User user)
-        {
-            if (user == null || string.IsNullOrEmpty(user.ds_email) || string.IsNullOrEmpty(user.ds_password))
-            {
-                return BadRequest("E-mail ou senha inválidos.");
-            }
-
-            var existingUser = await _userRepository.Login(user.ds_email);
-            if (existingUser == null || !BCrypt.Net.BCrypt.Verify(user.ds_password, existingUser.ds_password))
-            {
-                return BadRequest("E-mail ou senha inválidos.");
-            }
-
-            string token = CreateToken(existingUser);
-            return Ok(token);
-        }
-        */
-
-        [HttpPost("register")]
         public async Task<ActionResult<string>> Register([FromBody] UserRegisterDTO request)
         {
-
             try
             {
                 if (request == null)
                 {
-                    return BadRequest("Alguns dados estão inválidos, verifique!!");
+                    return BadRequest("Alguns dados estão inválidos, verifique!");
                 }
 
                 var createdUser = await _authService.RegisterAsync(request);
-
                 return CreatedAtAction(nameof(GetUserById), new
                 {
-                    userId = request
+                    userId = request.id_user
                 }, createdUser);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao adicionar dados no Banco de Dados");
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao adicionar dados no Banco de Dados: {ex.Message}");
             }
         }
-
-        [HttpPost("login")]
-        public async Task<ActionResult<string>> Login([FromBody] UserLoginDTO request)
-        {
-            return await _authService.LoginAsync(request);
-        }
-
-        /*private string CreateToken(User user)
-        {
-            List<Claim> claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Email, user.ds_email)
-            };
-
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("Jwt:Token").Value!));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-
-            var token = new JwtSecurityToken(
-                claims: claims,
-                expires: DateTime.Now.AddDays(1),
-                signingCredentials: creds
-            );
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
-        */
 
         /// <summary>
         /// Atualiza um usuário existente.
         /// </summary>
-        /// <param name="userId">O ID do usuário a ser atualizado.</param>
-        /// <param name="user">Os novos dados do usuário.</param>
-        /// <returns>Uma mensagem de sucesso.</returns>
+        /// <param name="userId">O ID do usuário no banco de dados.</param>
+        /// <param name="oldEmail">O e-mail antigo do usuário a ser atualizado.</param>
+        /// <param name="request">Os novos dados do usuário.</param>
+        /// <returns>O usuário atualizado.</returns>
         /// <response code="200">Usuário atualizado com sucesso.</response>
         /// <response code="404">Usuário não encontrado.</response>
         /// <response code="400">Dados inválidos.</response>
         /// <response code="500">Erro ao atualizar os dados no banco de dados.</response>
-        [HttpPut("{userId:int}")]
-        public async Task<ActionResult<string>> UpdateUser(int userId, [FromBody] UserUpdateDto user)
+        [HttpPut("{oldEmail}")]
+        public async Task<ActionResult<string>> UpdateUser(string oldEmail, [FromBody] UserUpdateDto request)
         {
             try
             {
-                if (user == null)
+                if (request == null)
                 {
-                    return BadRequest("Alguns dados estão inválidos, verifique!!");
+                    return BadRequest("Alguns dados estão inválidos, verifique!");
                 }
 
-                user.id_user = userId;
-                var updatedUser = await _userRepository.UpdateUser(user);
-                if (updatedUser == null)
-                {
-                    return NotFound($"Usuário com o ID: {userId}, não foi encontrado!");
-                }
-
-                return Ok("Usuário Atualizado com Sucesso!");
+                await _authService.UpdateUserByEmailAsync(oldEmail, request);
+                return Ok("Usuário Atualizado com Sucesso");
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao atualizar os dados no Banco de Dados");
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao atualizar os dados no Banco de Dados: {ex.Message}");
             }
         }
 
@@ -221,26 +167,20 @@ namespace ApiGreenway.Controllers
         /// </summary>
         /// <param name="userId">O ID do usuário a ser deletado.</param>
         /// <returns>Uma mensagem de sucesso.</returns>
-        /// <response code="200">Usuário deletado com sucesso.</response>
+        /// <response code="200">Usuário desativado com sucesso.</response>
         /// <response code="404">Usuário não encontrado.</response>
         /// <response code="500">Erro ao deletar os dados do banco de dados.</response>
-        [HttpDelete("{userId}")]
+        [HttpDelete("{userId:int}")]
         public async Task<ActionResult<string>> DeleteUser(int userId)
         {
             try
             {
-                var deletedUser = await _userRepository.GetUserById(userId);
-                if (deletedUser == null)
-                {
-                    return NotFound($"Usuário com o ID: {userId}, não foi encontrado!");
-                }
-
-                _userRepository.DeleteUser(userId);
-                return Ok("Usuário foi Deletado com Sucesso!");
+                _authService.DeleteAsync(userId);
+                return Ok("Usuário foi desativado com sucesso!");
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao deletar os dados do Banco de Dados");
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao deletar os dados do Banco de Dados: {ex.Message}");
             }
         }
     }
